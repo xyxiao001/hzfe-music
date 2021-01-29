42f91e6c27e33866f69ee401740f95e19d121134
xyxiao001 <463618622@qq.com>
2021-01-29 19:00:37 +0800

    build

---
+++ build/precache-manifest.fbec6a58b4ad4d47a7937b13025c1057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3ec4642d7d008c984eebef54de3a26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d46978918734a7a2f8",
+    "url": "/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "7b8b42da44978c794163",
+    "url": "/static/css/main.b60ee2d7.chunk.css"
+  },
+  {
+    "revision": "88d46978918734a7a2f8",
+    "url": "/static/js/2.d4cfe9da.chunk.js"
+  },
+  {
+    "revision": "7c1ef30a8cc271880293d345e033326d",
+    "url": "/static/js/2.d4cfe9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8b42da44978c794163",
+    "url": "/static/js/main.dc410ec5.chunk.js"
+  },
+  {
+    "revision": "4afa0ab719d812a3cc4d",
+    "url": "/static/js/runtime-main.08ca15ec.js"
+  }
+]);
