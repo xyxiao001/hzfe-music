dde638318de5580b31fb00b13a9d714e6ee98cf4
xyxiao001 <463618622@qq.com>
2021-06-16 21:07:28 +0800

    build and enlarge font-size

--- build/precache-manifest.3401624082177b97eb92157c49cbd6ec.js
+++ build/precache-manifest.3401624082177b97eb92157c49cbd6ec.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "320be341e7774c20c0351751b7b79ca2",
+    "revision": "69903837ceb82feedb2832bcd544d849",
     "url": "./index.html"
   },
   {
-    "revision": "f0953865594b59df5178",
-    "url": "./static/css/main.eb9b790a.chunk.css"
+    "revision": "f0dd15f1c5f7e117a98e",
+    "url": "./static/css/main.d27fee7c.chunk.css"
   },
   {
     "revision": "c3d0eb021c4ff648a4e6",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.66341679.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f0953865594b59df5178",
+    "revision": "f0dd15f1c5f7e117a98e",
     "url": "./static/js/main.9e5f8bd4.chunk.js"
   },
   {
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.85adbc76635b984ab49cc0ca39c39602.js"
+  "./precache-manifest.3401624082177b97eb92157c49cbd6ec.js"
 );
 
 self.addEventListener('message', (event) => {
